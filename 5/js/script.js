3647b5e0801146e5db11321b24c69c4189cb99d0
Giuseppe <giusepperaffa1988@gmail.com>
2020-12-18 16:32:32 +0100

    esercizi ok

--- 5/js/script.js
+++ 5/js/script.js
@@ -3,11 +3,10 @@
 //Il software stampa prima la parola più corta, poi la parola più lunga.
 
 //inserisco le mie 2 varibili direttamente con prompt e il valore numeroMaggiore
-var parolaUno = parseInt(prompt("inserisci una parola"));
-var parolaDue = parseInt(prompt("inseriscine un'altra"));
+var parolaUno = prompt("inserisci una parola");
+var parolaDue = prompt("inseriscine un'altra");
 var parolaPiuLunga;
-var pari = ("Le parole hanno pari lunghezza");
-
+var pari = "Le parole hanno pari lunghezza";
 
 if (parolaUno.length > parolaDue.length) {
   parolaPiuLunga = parolaUno;
