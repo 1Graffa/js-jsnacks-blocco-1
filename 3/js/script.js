ffb369b20b43c347e9a0e62e57e360572c01874e
Giuseppe <giusepperaffa1988@gmail.com>
2020-12-18 12:12:30 +0100

    3

--- 3/js/script.js
+++ 3/js/script.js
@@ -2,11 +2,14 @@
 //chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 //creo array coi nomi degli invitati
-var lista["Aldo", "Giovanni", "Giacomo", "Marina"];
-
+var lista["Aldo", "Giovanni", "Giacomo"];
+var nomeInput;
 //do il for per una volta sola al check del nome
 for (var i = 0; i < 1; i++){
   var nomeInput = prompt("Inserisci nome");
   if lista.includes(nomeInput){
-    
+    document.getElementById('messagex').innerHTML= "Si, sei stato invitato";
+    } else {
+    document.getElementById('messagey').innerHTML= "no, mi dispiace";
+    }
   }
