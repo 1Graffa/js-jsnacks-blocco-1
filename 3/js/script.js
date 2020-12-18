c2469da207ce530ad7da765d0e74172a843382c8
Giuseppe <giusepperaffa1988@gmail.com>
2020-12-18 12:45:59 +0100

    fine mattina

--- 3/js/script.js
+++ 3/js/script.js
@@ -4,13 +4,16 @@
 //creo array coi nomi degli invitati
 var lista = ["Aldo", "Giovanni", "Giacomo"];
 var nomeInput = prompt("Inserisci nome");
+//variabile sentinella
 var messaggio = false;
-//do il for per una volta sola al check del nome
 
+//diamo il for per ottenre una sola il check del nome
 for (var i = 0; i < lista.length; i++){
+  //se il nome corrusponte ad un nome della lista = true
   if (nomeInput == lista[i]){
     messaggio = true;
   }
+//messaggio 
 }
 if (messaggio){
   console.log("sei stato invitato");
