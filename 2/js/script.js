94dfb1b75ce2293d6ac26b1f63ccc9ae1ba27873
Giuseppe <giusepperaffa1988@gmail.com>
2020-12-18 12:05:28 +0100

    3 (bloccato)

--- 2/js/script.js
+++ 2/js/script.js
@@ -1,17 +1,16 @@
 //Crea un array vuoto.
 //Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
 // alert();
-// creo array vuoto + variabile numero
+// creo array vuoto + variabili numero e resto
 var lista = [];
 var numero;
 var resto = numero / 2;
 // creo la regola 6 numeri + l'inserisci numero con prompt
-for (var i=0; i<6; i++){
+for (var i = 0; i<6; i++){
 numero = prompt("Inserisci numero");
 // se resto diverso da 0 allora metti numero in array
 if (resto != 0){
   lista.push(numero);
   }
 }
-
- console.log(lista);
+console.log(lista);
---
+++ 3/js/script.js
@@ -0,0 +1,12 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby
+//chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+//creo array coi nomi degli invitati
+var lista["Aldo", "Giovanni", "Giacomo", "Marina"];
+
+//do il for per una volta sola al check del nome
+for (var i = 0; i < 1; i++){
+  var nomeInput = prompt("Inserisci nome");
+  if lista.includes(nomeInput){
+    
+  }
