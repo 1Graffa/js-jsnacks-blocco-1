3cdaae6fa348743b1257d84a130ba69bca0d6c2f
Giuseppe <giusepperaffa1988@gmail.com>
2020-12-18 11:45:05 +0100

    2

--- 2/js/script.js
+++ 2/js/script.js
@@ -1,14 +1,18 @@
 //Crea un array vuoto.
 //Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
-
-// creo array vuoto
-var numeri=[];
-
+// alert();
+// creo array vuoto + variabile numero
+var lista = [];
+var numero;
+var resto = numero / 2;
 // creo la regola 6 numeri + l'inserisci numero con prompt
-for(var i=0; i<6; i++);{
-numeri.push(prompt("Inserisci numero"));{
 
-  if(numeri[i]%2 != 0);{
+for (var i=0; i<6; i++){
+numero = prompt("Inserisci numero");
+// se resto diverso da 0 allora metti numero in array
+if (resto != 0){
+  lista.push(numero);
   }
 }
- console.log(numeri);
+
+ console.log(lista);
